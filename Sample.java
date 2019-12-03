8fd2ca87eb70f619fd8fc31f444732f18f9c71f1
kaushikgithub <50713680+kaushika66@users.noreply.github.com>
2019-12-03 23:22:27 +0530

    added Kaushik 3 time
    added Kaushik 3 time

--- Sample.java
+++ Sample.java
@@ -2,10 +2,10 @@ public class HelloGuys {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hey Agni");
+		System.out.println("Hey KAUSHIK ");
 		System.out.println("Hey Riya");
-		System.out.println("Hey Duck");
-		System.out.println("Hey Arya");//newly added
+		System.out.println("Hey KAUSHIK ");
+		System.out.println("Hey Arya");
 		System.out.println("Hey KAUSHIK ");
 	}
 
